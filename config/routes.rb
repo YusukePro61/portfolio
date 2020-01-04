9d32c909b6513b30d1afdd22e750a9abe734479c
YusukePro61 <learning630@gmail.com>
2020-01-04 23:41:55 +0900

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  resources :articles
+  root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
